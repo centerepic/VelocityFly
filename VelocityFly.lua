c9d48157b7cd8f393d69778cfdab078ea7001ae7
Sasha <83483477+centerepic@users.noreply.github.com>
2023-04-20 18:00:04 -0700

    Update VelocityFly.lua

--- VelocityFly.lua
+++ VelocityFly.lua
@@ -18,24 +18,32 @@ VelocityFly.LoopFunction = function()
         for _, KeyObject in pairs(UserInputService:GetKeysPressed()) do
             if KeyObject.KeyCode == Enum.KeyCode.W then
                 MovementInputDetected = true
-                VelocityFly.TargetCFrame = VelocityFly.TargetCFrame + Camera.CFrame.LookVector.Unit * VelocityFly.Speed
+                VelocityFly.TargetCFrame = VelocityFly.TargetCFrame + (Camera.CFrame.LookVector.Unit * VelocityFly.Speed)
             end
             if KeyObject.KeyCode == Enum.KeyCode.D then
                 MovementInputDetected = true
-                VelocityFly.TargetCFrame = VelocityFly.TargetCFrame + Camera.CFrame.LookVector:Cross(Vector3.new(0, 1, 0)).Unit * VelocityFly.Speed
+                VelocityFly.TargetCFrame = VelocityFly.TargetCFrame + (Camera.CFrame.LookVector:Cross(Vector3.new(0, 1, 0)).Unit * VelocityFly.Speed)
             end
             if KeyObject.KeyCode == Enum.KeyCode.A then
                 MovementInputDetected = true
-                VelocityFly.TargetCFrame = VelocityFly.TargetCFrame + Camera.CFrame.LookVector:Cross(Vector3.new(0, -1, 0)).Unit * VelocityFly.Speed
+                VelocityFly.TargetCFrame = VelocityFly.TargetCFrame + (Camera.CFrame.LookVector:Cross(Vector3.new(0, -1, 0)).Unit * VelocityFly.Speed)
             end
             if KeyObject.KeyCode == Enum.KeyCode.S then
                 MovementInputDetected = true
-                VelocityFly.TargetCFrame = VelocityFly.TargetCFrame + (Camera.CFrame.LookVector.Unit * - 1) * VelocityFly.Speed
+                VelocityFly.TargetCFrame = VelocityFly.TargetCFrame + ((Camera.CFrame.LookVector.Unit * - 1) * VelocityFly.Speed)
+            end
+            if KeyObject.KeyCode == Enum.KeyCode.E then
+                MovementInputDetected = true
+                VelocityFly.TargetCFrame = VelocityFly.TargetCFrame + (Camera.CFrame.UpVector.Unit * VelocityFly.Speed)
+            end
+            if KeyObject.KeyCode == Enum.KeyCode.Q then
+                MovementInputDetected = true
+                VelocityFly.TargetCFrame = VelocityFly.TargetCFrame + (Camera.CFrame.UpVector.Unit * -VelocityFly.Speed)
             end
         end
 
         if not MovementInputDetected then
-            VelocityFly.TargetCFrame = LocalPlayer.Character.HumanoidRootPart.CFrame + Vector3.new(0, 1.3 * (workspace.Gravity / 196.1999969482422), 0)
+            VelocityFly.TargetCFrame = LocalPlayer.Character.HumanoidRootPart.CFrame + Vector3.new(0, 1.5 * (workspace.Gravity / 196.1999969482422), 0)
         end
 
         LocalPlayer.Character.HumanoidRootPart.Velocity = (VelocityFly.TargetCFrame.Position - LocalPlayer.Character.HumanoidRootPart.Position)
